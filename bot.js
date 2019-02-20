51daf87d016f6ad124143679855ca5825f16c95b
Caglar Turali <caglarturali@gmail.com>
2019-02-20 17:24:38 +0300

    Revert back last change

--- bot.js
+++ bot.js
@@ -22,72 +22,71 @@ app.use(bodyParser.json());
 // We are receiving updates at the route below!
 app.post(`/bot${TOKEN}`, (req, res) => {
   bot.processUpdate(req.body);
-
-  // Listen for any kind of message.
-  bot.on('message', msg => {
-    const chatId = msg.chat.id;
-
-    // Send a generic message to the chat acknowledging receipt of their message.
-    bot.sendMessage(chatId, strings.t(msg, strings.MESSAGE));
-  });
-
-  let pendingQueries = [];
-
-  bot.on('inline_query', query => {
-    // Prompt user to start typing.
-    if (query.query === '') {
-      return respond.sendStaticResponse(
-        bot,
-        query,
-        '(^-^)',
-        strings.START_TYPING
-      );
-    }
-
-    // Find matching distros.
-    let matches = distrosData.filter(distroData => {
-      // Look for matches in both friendly name and url name.
-      const currentDistroName = distroData.distro_name.toLowerCase();
-      const currentDistroUrlName = distroData.url_name.toLowerCase();
-      const queryDistro = query.query.toLowerCase();
-
-      return (
-        currentDistroName.indexOf(queryDistro) >= 0 ||
-        currentDistroUrlName.indexOf(queryDistro) >= 0
-      );
-    });
-    if (matches.length > 10) {
-      matches.length = 10;
-    }
-
-    // Add maches to pending promises array and start fetching.
-    pendingQueries = [];
-    if (matches.length > 0) {
-      matches.forEach(match => {
-        pendingQueries.push(scrape.getDistroPopularity(match));
-      });
-
-      Promise.all(pendingQueries)
-        .then(results => respond.formatAndSendQueryResults(bot, query, results))
-        .catch(e => {
-          console.log(e.message);
-          respond.sendStaticResponse(bot, query, '(-.-)', strings.NO_RESULT);
-        });
-    } else {
-      respond.sendStaticResponse(bot, query, '(-.-)', strings.NO_RESULT);
-    }
-  });
-
-  bot.on('polling_error', error => {
-    console.log('Polling error:', error.code);
-  });
-
   res.sendStatus(200);
 });
 
 // Redirect all other requests to homepage of the bot.
 app.get('*', (req, res) => {
   res.redirect('https://t.me/WatcherCatBot');
+});
+
+// Listen for any kind of message.
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+
+  // Send a generic message to the chat acknowledging receipt of their message.
+  bot.sendMessage(chatId, strings.t(msg, strings.MESSAGE));
+});
+
+let pendingQueries = [];
+
+bot.on('inline_query', query => {
+  // Prompt user to start typing.
+  if (query.query === '') {
+    return respond.sendStaticResponse(
+      bot,
+      query,
+      '(^-^)',
+      strings.START_TYPING
+    );
+  }
+
+  // Find matching distros.
+  let matches = distrosData.filter(distroData => {
+    // Look for matches in both friendly name and url name.
+    const currentDistroName = distroData.distro_name.toLowerCase();
+    const currentDistroUrlName = distroData.url_name.toLowerCase();
+    const queryDistro = query.query.toLowerCase();
+
+    return (
+      currentDistroName.indexOf(queryDistro) >= 0 ||
+      currentDistroUrlName.indexOf(queryDistro) >= 0
+    );
+  });
+  if (matches.length > 10) {
+    matches.length = 10;
+  }
+
+  // Add maches to pending promises array and start fetching.
+  pendingQueries = [];
+  if (matches.length > 0) {
+    matches.forEach(match => {
+      pendingQueries.push(scrape.getDistroPopularity(match));
+    });
+
+    Promise.all(pendingQueries)
+      .then(results => respond.formatAndSendQueryResults(bot, query, results))
+      .catch(e => {
+        console.log(e.message);
+        respond.sendStaticResponse(bot, query, '(-.-)', strings.NO_RESULT);
+      });
+  } else {
+    respond.sendStaticResponse(bot, query, '(-.-)', strings.NO_RESULT);
+  }
+});
+
+bot.on('polling_error', error => {
+  console.log('Polling error:', error.code);
 });
 
 module.exports = app;
